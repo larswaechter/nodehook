07a179c9e0900f8e6d36f608f95bca6b382e6762
Lars Wächter <lawaendel@googlemail.com>
2018-11-05 18:55:51 +0100

    Remove dist directory

--- dist/config/globals.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// global variable
-exports.variables = {
-    env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3000
-};
-//# sourceMappingURL=globals.js.map
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-// init express server
-var app = new server_1.Server();
-app.start();
-//# sourceMappingURL=index.js.map
